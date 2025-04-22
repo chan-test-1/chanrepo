e5ac3a776e68c5c64ba3e9665e8623cbcf807a6e
jihoon120 <eashoon1@naver.com>
2025-04-22 23:43:53 +0900

    4월 22일 공부 업데이트

---
+++ javaScript/day01/console.js
@@ -0,0 +1,11 @@
+let a = 5; // 선언, 할당
+console.log(a) // 사용, 5
+console.log(a + "입니다") // 5입니다  
+console.log(`${a} 입니다.`) // 5입니다  백틱은 변수를 ${} 를 써서 변수를 가져올수있고 한글쓰면 그냥 문자열.
+
+//색상은 브라우저에서만 변화.
+console.warn("warn")
+console.error("error")
+console.info("info")
+
+
