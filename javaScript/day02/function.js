5262e55f12778c61d013f2912d3f655ceccbc930
jihoon120 <eashoon1@naver.com>
2025-04-25 00:30:42 +0900

    4월 6일 2:02:00

---
+++ javaScript/day02/function.js
@@ -0,0 +1,61 @@
+// 사용자의 투임금액 -> 바뀔 수 있다.
+// isError -> 바뀔 수 있다.
+// price -> 내부적인 실행 코드가 바뀌지 않으면 바뀌지 않는 값.
+// charege -> 내부적으로 연산되는 값.
+
+
+// 지역변수, 전역변수, 매개변수(parameter)
+
+// 지역변수는 특정 영역 안.
+// 전역변수는 그냥 우리가 선언하는것 어디서든 다씀.
+// 매개변수는 값을 넘기고 받아서 쓰는 것. 특정함수에서 유효함. (지역변수처럼 움직임).
+function coke(coin, isError ) {
+    const price = 1000;
+    const charge = coin - price;
+
+    if (isError || coin < price) {   
+        return console.log('코인', coin); 
+    } 
+    
+    if (coin === price){ 
+        return console.log('콜라') // 콜라만 출력
+    }
+    
+    if (price < coin) { 
+        return console.log('콜라', charge)
+    } 
+}
+
+// console.log(price) // 함수내에서 선언한건 쓸 수없으므로 price is not defined 에러뜬다. 이오류가뜨면 절대안된다. 내가 생각안하고 따라쓰고있다는것.
+coke(5000, false)
+coke(1000, false)
+coke(500, false)
+coke(1000, true)
+
+// 다시 한번 말하지만 자바스크립트 함수 생성의 주체는 본인.
+// 내가 필요한 갑승ㄹ 지정, 내가 원하는 값을 출력
+// 모범답안이 없다. 강사가 지정한게 아니다!
+
+
+/*  기존소스
+
+let isError = true;
+const coin = 5000;
+const price = 1000;
+const charge = coin - price;
+
+
+if (isError || coin < price) {   
+    return console.log('코인', coin); 
+} 
+
+if (coin === price){ 
+    return console.log('콜라') // 콜라만 출력
+}
+
+if (price < coin) { 
+    return console.log('콜라', charge)
+} 
+*/
+
+
